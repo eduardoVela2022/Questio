46c52bde0fbcb139f0cf18f5b38871be5068a043
eduardoVela <eduardovela2022@icloud.com>
2024-06-26 09:57:18 -0600

    Added code to the connection.js file, so that a connection with the database can be established.

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,22 @@
-// TODO: Connection.js file
+// Imports
+require("dotenv").config();
+const Sequelize = require("sequelize");
+
+// Database connection
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USERNAME,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "postgres",
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
+
+// Exports
+module.exports = sequelize;
